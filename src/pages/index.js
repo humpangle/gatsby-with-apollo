2d9980dd7a22ae8bf20ef139f0a9e56c0528ff5a
maneptha <maneptha@gmail.com>
2019-03-17 03:35:33 +0100

    use apollo higher order component

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,10 +2,11 @@ import React from 'react';
 import { graphql } from 'gatsby';
 import { Query } from 'react-apollo';
 import gql from 'graphql-tag';
+import { graphql as apolloGraphql } from 'react-apollo';
 
 // This query is executed at build time by Gatsby.
 export const GatsbyQuery = graphql`
-  {
+  query {
     rickAndMorty {
       character(id: 1) {
         name
@@ -17,18 +18,21 @@ export const GatsbyQuery = graphql`
 
 // This query is executed at run time by Apollo.
 const APOLLO_QUERY = gql`
-  {
-    dog(breed: "frise") {
-      breed
-      displayImage
+  query {
+    character(id: 1) {
+      id
+      name
+      image
     }
   }
 `;
 
-export default ({
+export const Comp = ({
   data: {
     rickAndMorty: { character },
   },
+
+  apolloQuery: { character: characterFromApollo, loading, error },
 }) => (
   <div style={{ textAlign: 'center', width: '600px', margin: '50px auto' }}>
     <h1>{character.name} With His Pupper</h1>
@@ -38,15 +42,17 @@ export default ({
     <div>
       <img src={character.image} alt={character.name} style={{ width: 300 }} />
 
-      <Query query={APOLLO_QUERY}>
-        {({ data, loading, error }) => {
-          if (loading) return <p>Loading pupper...</p>;
-          if (error) return <p>Error: ${error.message}</p>;
+      {(function() {
+        if (loading) return <p>Loading pupper...</p>;
+        if (error) return <p>Error: ${error.message}</p>;
 
-          const { displayImage: src, breed } = data.dog;
-          return <img src={src} alt={breed} style={{ maxWidth: 300 }} />;
-        }}
-      </Query>
+        const { image: src, name } = characterFromApollo;
+        return <img src={src} alt={name} style={{ maxWidth: 300 }} />;
+      })()}
     </div>
   </div>
 );
+
+export default apolloGraphql(APOLLO_QUERY, {
+  props: props => ({ apolloQuery: props.data }),
+})(Comp);
