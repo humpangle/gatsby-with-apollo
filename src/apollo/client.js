760dc41db9530baf30d5af859a9b56eca65c4c4b
maneptha <maneptha@gmail.com>
2019-03-17 03:35:20 +0100

    Change to working apollo client url

--- src/apollo/client.js
+++ src/apollo/client.js
@@ -2,6 +2,9 @@ import ApolloClient from 'apollo-boost';
 import fetch from 'isomorphic-fetch';
 
 export const client = new ApolloClient({
-  uri: 'https://nx9zvp49q7.lp.gql.zone/graphql',
+  uri: 'https://rickandmortyapi.com/graphql',
   fetch,
+  fetchOptions: {
+    'Access-Control-Allow-Origin': '*',
+  },
 });
